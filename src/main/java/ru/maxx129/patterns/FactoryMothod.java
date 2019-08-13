e1cd45f6f1319c4697a62660b22a79dbe9525195
maxx129 <maxx129@yandex.ru>
2019-08-13 14:15:05 +0300

    130819 1415

--- src/main/java/ru/maxx129/patterns/FactoryMothod.java
+++ src/main/java/ru/maxx129/patterns/FactoryMothod.java
@@ -3,43 +3,65 @@ package ru.maxx129.patterns;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FactoryMothod {
 
-    public static void main(String[] args) {
-        List<Chocolate> chocolate = new ArrayList();
-    }
-}
 
 
 interface Chocolate {
-    void printPrise();
+    void printType();
 }
 
 class BlackChocolate implements Chocolate {
-    private final int prise = 10;
+    private final String type = "Black";
 
     @Override
-    public void printPrise() {
-        System.out.println(prise);
+    public void printType() {
+        System.out.println(type);
     }
 }
 
 class WhiteChocolate implements Chocolate {
-    private final int prise = 15;
+    private final String type = "White";
 
     @Override
-    public void printPrise() {
-        System.out.println(prise);
+    public void printType() {
+        System.out.println(type);
     }
 }
 
 
 abstract class Factory {
-    private int flag;
+    public int flag;
 
     Factory(int flag) {
         this.flag = flag;
     }
 
     abstract Chocolate create();
+}
+
+class CreatorFactory extends Factory {
+
+    CreatorFactory(int flag) {
+        super(flag);
+    }
+
+    @Override
+    Chocolate create() {
+        if(flag ==0) {
+            return new BlackChocolate();
+        }
+        if(flag == 1) {
+            return new WhiteChocolate();
+        }
+        throw new IllegalArgumentException();
+    }
+}
+
+public class FactoryMothod {
+
+    public static void main(String[] args) {
+        List<Chocolate> chocolate = new ArrayList();
+        Factory factory = new CreatorFactory(2);
+        factory.create().printType();
+    }
 }
